1671facdb93478776519c3bfd960d01f0fef98b9
Brent Kirby <brent@kurbmedia.com>
2012-12-03 22:16:59 -0500

    valudate name and domain, use identifiers

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,10 +2,23 @@ class Site
   include Mongoid::Document
   include Mongoid::Timestamps
   
-  field :name,      :type => String
-  field :domain,    :type => String
-  field :subdomain, :type => Boolean, :default => false
+  field :name,        :type => String
+  field :domain,      :type => String
+  field :subdomain,   :type => Boolean, :default => false
+  field :identifier,  :type => String
   
+  validates :name, :domain, :presence => true
+  before_create :generate_identifier
   has_many :pages
+  
+  private
+  
+  ##
+  # Create a shortened identifier to easily 
+  # identify this site.
+  # 
+  def generate_identifier
+    self.identifier ||= self.name.parameterize.underscore
+  end
   
 end
